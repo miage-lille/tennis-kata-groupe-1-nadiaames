c4d36bad3b8edab824ab04b7de40ba582531247d
Nadia Amesrouy <nadia.amesrouy.etu@univ-lille.fr>
2025-02-04 03:22:34 +0100

    updated

--- types/score.ts
+++ types/score.ts
@@ -1,11 +1,14 @@
-import { Player } from './player';
+import { Player} from './player';
 
-export type Point = number;
+export type Point = Love | Fifteen | Thirty;
 
-export const love = (): Point => 0;
-export const fifteen = (): Point => 15;
-export const thirty = (): Point => 30;
-export const fortyPoints = (): Point => 40;
+export type Love = { kind: 'LOVE'; };
+export type Fifteen = { kind: 'FIFTEEN'; };
+export type Thirty = { kind: 'THIRTY'; };
+
+export const love = (): Love => ({ kind: 'LOVE' });
+export const fifteen = (): Fifteen => ({ kind: 'FIFTEEN' });
+export const thirty = (): Thirty => ({ kind: 'THIRTY' });
 
 export type Deuce = {
   kind: 'DEUCE';
@@ -15,17 +18,34 @@ export const deuce = (): Deuce => ({
   kind: 'DEUCE',
 });
 
-export type Forty = {
-  kind: 'FORTY';
-  leadingPlayer: Player;
-  trailingPlayerPoints: Point;
+export type FortyData = {
+  player: Player;
+  otherPoint: Point; 
 };
 
-export const forty = (leadingPlayer: Player, trailingPlayerPoints: Point): Forty => ({
+export type Forty = {
+  kind: 'FORTY';
+  fortyData: FortyData;
+};
+
+export const forty = (player: Player, otherPoint: Point): Forty => ({
   kind: 'FORTY',
-  leadingPlayer,
-  trailingPlayerPoints,
-});
+  fortyData: {player, otherPoint}
+})
+
+export type PointsData = {
+  PLAYER_ONE: Point;
+  PLAYER_TWO: Point;
+};
+
+export type Points = {
+  kind: 'POINTS';
+  pointsData: PointsData;
+};
+
+const s1: PointsData = { PLAYER_ONE: love(), PLAYER_TWO: love() };
+const s2: PointsData = { PLAYER_ONE: fifteen(), PLAYER_TWO: love() };
+const s3: PointsData = { PLAYER_ONE: thirty(), PLAYER_TWO: love() };
 
 export type Advantage = {
   kind: 'ADVANTAGE';
@@ -45,27 +65,6 @@ export type Game = {
 export const game = (winner: Player): Game => ({
   kind: 'GAME',
   player: winner,
-});
-
-export type PointsData = {
-  PLAYER_ONE: Point;
-  PLAYER_TWO: Point;
-};
-
-export type Points = {
-  kind: 'POINTS';
-  pointsData: PointsData;
-};
-
-export const points = (
-  playerOnePoints: Point,
-  playerTwoPoints: Point
-): Points => ({
-  kind: 'POINTS',
-  pointsData: {
-    PLAYER_ONE: playerOnePoints,
-    PLAYER_TWO: playerTwoPoints,
-  },
 });
 
 export type Score = Points | Deuce | Forty | Advantage | Game;
